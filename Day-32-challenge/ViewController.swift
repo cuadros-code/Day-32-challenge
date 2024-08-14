db343aa1a6c2e06d266b956d1310a64c208db828
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-14 12:13:17 -0500

    add table, bar actions

--- Day-32-challenge/ViewController.swift
+++ Day-32-challenge/ViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var textList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "List"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(openAlert)
+        )
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .trash,
+            target: self,
+            action: #selector(resetTable)
+        )
+    }
+    
+    @objc func openAlert() {
+        
+        let ac = UIAlertController(
+            title: "",
+            message: nil,
+            preferredStyle: .alert
+        )
+        ac.addTextField()
+        
+    }
+    
+    @objc func resetTable(){
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = textList[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
     }
 
 
