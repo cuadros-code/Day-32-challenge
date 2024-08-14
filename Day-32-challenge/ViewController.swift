77e9ba9328c8df5c1221451df8a0cc60f5ff8220
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-14 17:04:37 -0500

    Add context menu keep pressed, alert confimation delete items

--- Day-32-challenge/ViewController.swift
+++ Day-32-challenge/ViewController.swift
@@ -27,21 +27,51 @@ class ViewController: UITableViewController {
             target: self,
             action: #selector(resetTable)
         )
+        
+        textList = ["1", "2", "3", "4"]
     }
     
     @objc func openAlert() {
         
-        let ac = UIAlertController(
-            title: "",
+        let alertController = UIAlertController(
+            title: "Add Item",
             message: nil,
             preferredStyle: .alert
         )
-        ac.addTextField()
+        alertController.addTextField()
         
+        let action = UIAlertAction(title: "Submit", style: .default) { [ weak self, weak alertController] _ in
+            guard let text = alertController?.textFields?[0].text else { return }
+            self?.addText(text: text)
+        }
+        let cancelAction = UIAlertAction(
+            title: "Cancel",
+            style: .cancel
+        )
+        alertController.addAction(action)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
     }
     
     @objc func resetTable(){
-        tableView.reloadData()
+        let ac = UIAlertController(
+            title: "Delete",
+            message: "Are you sure to delete all the elements?",
+            preferredStyle: .actionSheet
+        )
+        
+        let confirmAction = UIAlertAction(title: "Delete", style: .default) { [weak self] _ in
+            self?.textList.removeAll()
+            self?.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        cancelAction.setValue(UIColor.red, forKey: "titleTextColor")
+        
+        ac.addAction(confirmAction)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -55,6 +85,52 @@ class ViewController: UITableViewController {
         cell.contentConfiguration = content
         return cell
     }
+    
+    func addText(text: String){
+        textList.insert(text, at: 0)
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        contextMenuConfigurationForRowAt indexPath: IndexPath,
+        point: CGPoint) -> UIContextMenuConfiguration? {
+       
+        let deleteAction = UIAction(
+            title: "Delete",
+            image: UIImage(systemName: "minus.circle")) { [weak self] _ in
+            self?.deleteItem(indexRow: indexPath.row)
+        }
+        
+        let shareAction = UIAction(
+            title: "Share",
+            image: UIImage(systemName: "square.and.arrow.up")) { [weak self] _ in
+            self?.sharedItem(indexRow: indexPath.row)
+        }
+        
+        let menu = UIMenu(title: "", children: [deleteAction, shareAction])
+        
+        return UIContextMenuConfiguration(identifier: nil, actionProvider:  { _ in
+            return menu
+        })
+    }
+    
+    func deleteItem(indexRow: Int){
+        textList.remove(at: indexRow)
+        tableView.deleteRows(at: [IndexPath(row: indexRow, section: 0)], with: .bottom)
+    }
+    
+    func sharedItem(indexRow: Int){
+        let textRow = textList[indexRow]
+        let ac = UIActivityViewController(
+            activityItems: [textRow],
+            applicationActivities: nil
+        )
+        
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+
 
 
 }
